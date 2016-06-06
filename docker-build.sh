cd035a543e71fcd09c394542b839635b52174249
Divan Visagie <visagiedivan@gmail.com>
2016-06-06 20:32:15 +0200

    dammit travis

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sbt server/docker:stage
-docker build -t divanvisagie/writ-api target/docker/stage
-docker push divanvisagie/writ-api
+docker build -t divanvisagie/writ-api:latest target/docker/stage
+docker push divanvisagie/writ-api:latest
