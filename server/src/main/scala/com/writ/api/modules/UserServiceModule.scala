1495a20773440756e1add88b654bf22682e63ef4
Divan Visagie <visagiedivan@gmail.com>
2016-07-02 16:15:36 +0200

    improve env names

--- server/src/main/scala/com/writ/api/modules/UserServiceModule.scala
+++ server/src/main/scala/com/writ/api/modules/UserServiceModule.scala
@@ -3,19 +3,33 @@ package com.writ.api.modules
 import com.google.inject.{Provides, Singleton}
 import com.swissguard.user.thriftscala.UserService
 import com.twitter.finagle.ThriftMux
-import com.twitter.finagle.stats.NullStatsReceiver
-import com.twitter.finagle.tracing.NullTracer
+import com.twitter.finagle.stats.DefaultStatsReceiver
+import com.twitter.finagle.zipkin.thrift.ZipkinTracer
 import com.twitter.inject.TwitterModule
 import com.twitter.util.Future
 
 object UserServiceModule extends TwitterModule {
 
-  val inet = sys.env.getOrElse("WRIT_API_SWISSGUARD", "localhost:8888")
+  val inet = sys.env.getOrElse(
+    "SWISSGUARD_HOST",
+    "localhost:8888,localhost:7777"
+  )
 
+  val receiver = DefaultStatsReceiver.get
+
+  val zipkinHost = sys.env.getOrElse("ZIPKIN_HOST","localhost")
+  val zipkinPort = sys.env.getOrElse("ZIPKIN_PORT", "9410").toInt
+
+
+  val tracer = ZipkinTracer.mk(
+    host = zipkinHost,
+    port = zipkinPort,
+    statsReceiver = receiver,
+  )
   @Singleton @Provides
   def providesUserServiceClient(): UserService[Future] =
     ThriftMux.client
-    .withTracer(NullTracer)
-    .withStatsReceiver(NullStatsReceiver)
+    .withTracer(tracer)
+    .withStatsReceiver(receiver)
     .newIface[UserService.FutureIface](inet)
 }
